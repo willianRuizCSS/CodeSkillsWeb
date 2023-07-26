2244969c7c0e00361f7b4bcb898785bc1e5e56a3
Cristian Javier Padierna Higuita <122125990+CristianPadiernaCSS@users.noreply.github.com>
2023-07-26 12:34:02 -0500

    Pagina web Code Skills

---
+++ contactform/contactform.php
@@ -0,0 +1,25 @@
+<?php 
+
+$nombre = $_POST['name'];
+$email = $_POST['email'];
+$telefono = $_POST['phone'];
+$mensaje = $_POST['message'];
+$asunto = $_POST['subject']
+
+$header = 'from: '.$mail."\r\n";
+$header .= "X-Mailer:PHP/".phpversion()."\r\n";
+$header .= "Mime-Version:1.0 \r\n";
+$header .= "content-Type:text/plain";
+
+$message = "Este mensaje fue enviado por: ".$nombre."\r\n";
+$message .= "Su e-mail es: ".$email."\r\n";
+// $message .= "Telefono: ".$telefono."\r\n";
+$message .= "mensaje: ".$mensaje."\r\n";
+$message .= "enviado el: ".date('d/m/Y',time());
+
+//ceo@codeskills.com.co
+$para = 'cristian.j96@hotmail.com';
+
+if(mail($para,$asunto,utf8_decode($message),$header))
+echo "<script type='text/javascript'>alert('Tu message ha sido enviado exitosamente');</script>";
+ ?>
